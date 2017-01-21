4af79368860a44ee15edf26f147a8f5ee7e98063
James Taggart <jxt1607@g.rit.edu>
2017-01-20 23:51:02 -0500

    Create Materials for Buildings. Create building generation script. Shenanigans.

---
+++ WaveForWaves/Assets/Scripts/GenBuildingAtPoint.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenBuildingAtPoint : MonoBehaviour {
+
+    public Transform spawnPoint;
+
+    public GameObject botFloor;
+    public GameObject midFloor;
+    public GameObject topFloor;
+    /// <summary>
+    /// Minimum of 3 for buildings.
+    /// </summary>
+    public int buildingHeight;
+    private float currentHeight;
+    // Use this for initialization
+    void Start () {
+
+        if(buildingHeight < 3)
+        {
+            buildingHeight = 3;
+        }
+
+        //First, generate the bottom floor at the position of the spawnPoint
+        Instantiate(botFloor, spawnPoint.transform.position,spawnPoint.transform.rotation);
+        currentHeight = spawnPoint.transform.position.y;
+        print(spawnPoint.transform.position.y);
+        for(int i = 0; i < buildingHeight-2; i++)
+       { 
+            spawnPoint.transform.position = new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y + 1f, spawnPoint.transform.position.z);
+            Instantiate(midFloor, spawnPoint.transform.position,spawnPoint.transform.rotation);
+            print(spawnPoint.transform.position.y);
+        }
+        spawnPoint.transform.position = new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y + 1f, spawnPoint.transform.position.z);
+        print(spawnPoint.transform.position.y);
+        Instantiate(topFloor, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+    }
+	
+	// Update is called once per frameS
+	void Update () {
+	
+	}
+}
