761ac3a1ded5434085a762c0065b7edd52826d36
Reuben Brenner-Adams <rmb1952@rit.edu>
2017-01-20 22:52:20 -0500

    Added new Unity project (3D). Added 3D Leap Motion hand controller demo.

---
+++ WaveForWaves3D/Assets/Scripts/DetectHands.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class DetectHands : MonoBehaviour
+{
+	Controller controller;
+	Frame currentFrame;
+	Hand interactionHand;
+
+	public GameObject cube;
+
+	void Awake()
+	{
+		controller = new Controller();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		currentFrame = controller.Frame();
+		interactionHand = currentFrame.Hands [0];
+		cube.transform.position = new Vector3(interactionHand.PalmPosition.x / 20f, interactionHand.PalmPosition.y / 40f - 5f, interactionHand.PalmPosition.z / -30f);
+	}
+}
