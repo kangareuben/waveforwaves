08640311c43e7ecb98c74121275b4bde8fd94190
Reuben Brenner-Adams <rmb1952@rit.edu>
2017-01-20 21:52:18 -0500

    Added simple Leap Motion hand controller demo (feat. cube)

---
+++ WaveForWaves/Assets/Scripts/DetectHands.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class DetectHands : MonoBehaviour
+{
+	Controller controller;
+	Frame currentFrame;
+
+	public GameObject cube;
+
+	void Awake()
+	{
+		controller = new Controller();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		currentFrame = controller.Frame();
+		cube.transform.position = new Vector3(currentFrame.Hands[0].PalmPosition.x / 20f, currentFrame.Hands[0].PalmPosition.y / 50f - 5f, 0);
+	}
+}
