573e18f0693fc630a81fd0f3838ec02d4f65edc9
Reuben Brenner-Adams <rmb1952@rit.edu>
2017-01-21 19:21:16 -0500

    Added killzone

---
+++ WaveForWaves3D/Assets/Scripts/KillZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Destroy (other.gameObject);
+	}
+}
